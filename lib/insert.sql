74b060c24d837f492bd7523e1b9988b3bce06506
Favor Vincent <vfavorm77@gmail.com>
2024-06-02 09:49:43 +0300

    bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "Gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 56, "F", "black", "Gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 16, "M", "brown", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "white", "Curious", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "white", "Solitary", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 24, "F", "black", "Shy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 14, "F", "brown", "Calm", 1);
